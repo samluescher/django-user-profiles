dd9ba769ccfaa280125a528f0ffe049b323d0d0a
philomat <philomat@popkultur.net>
2011-08-19 20:36:49 -0400

    Removed PerRequestCacheMiddleware because it's outside the scope of this app

--- user_profiles/middleware.py
+++ user_profiles/middleware.py
@@ -1,5 +1,3 @@
-import datetime
-
 try:
     from threading import local
 except ImportError:
@@ -8,8 +6,10 @@ except ImportError:
 _thread_locals = local()
 
 class CurrentUserMiddleware(object):
-    """Middleware that gets user object from the
-    request object and saves it in thread local storage."""
+    """
+    Middleware that gets user object from the
+    request object and saves it in thread local storage.
+    """
 
     def process_request(self, request):
         _thread_locals.user = getattr(request, 'user', None)
@@ -21,23 +21,3 @@ class CurrentUserMiddleware(object):
     @staticmethod
     def get_current_user_groups():
         return CurrentUserMiddleware.get_current_user().groups.all()
-
-class PerRequestCacheMiddleware(object):
-    """Middleware 
-    request object and saves it in thread local storage."""
-
-    def process_request(self, request):
-        _thread_locals.now = datetime.datetime.now()
-        _thread_locals.cache = {}
-
-    @staticmethod
-    def get(key, default=None):
-        return _thread_locals.cache.get(key, default)
-
-    @staticmethod
-    def set(key, value):
-        _thread_locals.cache[key] = value
-
-    @staticmethod
-    def now():
-        return _thread_locals.now
